a015a5965e56c628211f9f0e85bed5ccd4581b86
Natan Farkash <natqe3@gmail.com>
2018-11-05 17:28:12 +0200

    omdb key from url & add meta tag Content Security Policy

--- src/environments/environment.general.ts
+++ src/environments/environment.general.ts
@@ -1,6 +1,4 @@
-const APP_NAME = 'You Say'
-
 export const general = {
-  OMDB_API_KEY:  '7ebce949',
+  OMDB_API_KEY:  new URL(location.href).searchParams.get('omdbkey'),
   OMDB_API_ORIGIN:  'http://www.omdbapi.com/',
 }
